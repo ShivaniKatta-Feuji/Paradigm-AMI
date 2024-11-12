97c9feec3a6027dd39af2bc2edd66965540cfd71
ShivaniKatta-Feuji <shivani.katta@feuji.com>
2024-11-12 10:50:10 +0530

    Folder structure

---
+++ tests/paradigm.spec.js
@@ -0,0 +1,10 @@
+const { test } = require('@playwright/test')
+const indexPage  =require('../index.page')
+require('dotenv').config
+
+
+test.describe("Paradigm Test Cases",async()=>{
+    test.beforeEach("Launch URL",async ({page})=>{
+
+    })
+})
