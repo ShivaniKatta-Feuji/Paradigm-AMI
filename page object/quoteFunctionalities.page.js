ba1571b99620d030fe257af5a4051c8e75a9cc28
vaishaligoje <vaishali.goje@feuji.com>
2024-11-13 18:43:22 +0530

    my quote functionalities

--- page object/quoteFunctionalities.page.js
+++ page object/quoteFunctionalities.page.js
@@ -1,17 +1,16 @@
 const {expect}=require('@playwright/test')
 const data=require('../data/data.json')
 const helper=require("../utils/helper")
-
+require('dotenv').config()
 
 exports.QuoteFunctionality=class QuoteFunctionality{
     constructor(page){
         this.page=page
-        this.myquote=page.locator("//li[@class='nav-item ms-1 quotes active']")
         this.select=page.locator("#select-addon")
-        this.search=page.locator("//input[@class='form-control w-auto']")
+        this.search=page.locator("//div[@class='input-group']//input")
         this.show=page.locator("#perPage")
         this.newquote=page.locator("//a[@class='btn btn-success']")
-        this.nodata=page.locator(".not-found")
+        this.nodataCheck=page.locator(".not-found")
         this.deleteBtn=page.locator("(//button[@title='Delete'])[1]")
         this.show=page.locator("#perPage")
         this.snackbar=page.locator(".top-right")
@@ -24,88 +23,75 @@ exports.QuoteFunctionality=class QuoteFunctionality{
         this.password=page.locator("//input[@id='password']");
         this.nextButton=page.locator("//span[text()='Next']//parent::button");
         this.signInButton=page.locator("//span[text()='Sign In']//parent::button");
-
         this.myQuotes=page.locator("//div[@id='navbarSupportedContent']//a[contains(@href,'quotes')]");
         this.newQuote=page.locator("//span[text()='New Quote']");
     }
     async launchURL(){
         await this.page.goto("https://am-webcp-test.myparadigmcloud.com/");
-
-        
         await this.username.fill("AMI-AutoInternal@myparadigm.testinator.com");
         await this.nextButton.click();
         await this.password.fill("@miAt0Te$t3049");
         await this.signInButton.click();
         await this.myQuotes.click();
     }
-    async searchByNumber(){
-        await this.select.selectOption({value:'QuoteNumber'})
+    async searchByNumber(){ 
+        await this.select.selectOption({value:data.selectNumberOption})
         await this.search.fill(data.number)
         await expect(this.checkNumberColumn).toHaveText(data.number)
     }
     async searchByInvalidNumber(){
-        await this.select.selectOption({value:'QuoteNumber'})
+        await this.select.selectOption({value:data.selectNumberOption})
         await this.search.fill(data.invalidNumber)
-        expect(this.nodata).toHaveText("No data found")
+        expect(this.nodataCheck).toHaveText(data.noDataText)
     }
     async searchByName(){
-        await this.select.selectOption({value:'QuoteName'})
+        await this.select.selectOption({value:data.selectNameOption})
         await this.search.fill(data.name)
-        await this.page.waitForTimeout(data.smalltimeout)
+        await this.page.waitForTimeout(Number(process.env.smallTimeout))
         const recordsCount=await this.row.count()
-        for (let index = 0; index < recordsCount; index++) {
-            const nameColumn=await this.row.nth(index).locator('td:nth-child(4)').textContent()
-            expect(nameColumn.trim()).toBe(data.name);
-        }
+        helper.validateTable(this.row,data.name,data.namePosition)
     }
     async searchByInvalidName(){
-        await this.select.selectOption({value:'QuoteName'})
+        await this.select.selectOption({value:data.selectNameOption})
         await this.search.fill(data.invalidName)
-        expect(this.nodata).toHaveText("No data found")
+        expect(this.nodataCheck).toHaveText(data.noDataText)
     }
     async searchByClient(){
-        await this.select.selectOption({value:'ClientName'})
+        await this.select.selectOption({value:data.selectClientOption})
         await this.search.fill(data.client)
-        await this.page.waitForTimeout(data.smalltimeout)
+        await this.page.waitForTimeout(Number(process.env.smallTimeout))
         const recordsCount=await this.row.count()
-        for (let index = 0; index < recordsCount; index++) {
-            const clientColumn=await this.row.nth(index).locator('td:nth-child(5)').textContent()
-            expect(clientColumn.trim()).toBe(data.client);
-        }
-        
+        helper.validateTable(this.row,data.client,data.clientPosition)  
     }
     async searchByInvalidClient(){
-        await this.select.selectOption({value:'ClientName'})
+        await this.select.selectOption({value:data.selectClientOption})
         await this.search.fill(data.invalidClient)
-        expect(this.nodata).toHaveText("No data found")
+        expect(this.nodataCheck).toHaveText(data.noDataText)
     }
     async searchByPONumber(){
-        await this.select.selectOption({value:'PONumber'})
+        await this.select.selectOption({value:data.selectPONumberOption})
         await this.search.fill(data.PONumber)
-        await this.page.waitForTimeout(data.smalltimeout)
+        await this.page.waitForTimeout(Number(process.env.smallTimeout))
         const recordsCount=await this.row.count()
-        for (let index = 0; index < recordsCount; index++) {
-            const poNumber=await this.row.nth(index).locator('td:nth-child(7)').textContent()
-            expect(poNumber.trim()).toBe(data.PONumber);
-        }
+        helper.validateTable(this.row,data.PONumber,data.PONumberPosition)
     }
     async searchByInvalidPONumber(){
-        await this.select.selectOption({value:'PONumber'}) 
+        await this.select.selectOption({value:data.selectPONumberOption}) 
         await this.search.fill(data.invalidPONumber)
-        expect(this.nodata).toHaveText("No data found")
+        expect(this.nodataCheck).toHaveText(data.noDataText)
     }
     async pagination(){
-        await this.show.selectOption({value:'25'})
+        await this.show.selectOption({value:data.paginationValue})
     }
     async newQuoteCreate(){
         await this.newquote.click()
-        expect(this.page).toHaveURL("https://am-webcp-test.myparadigmcloud.com/quotes/create")
+        expect(this.page).toHaveURL(data.newQuoteURL)
     }
     async delete(){
         await this.deleteBtn.click()
         helper.alertHandling(this.page)
-        await this.page.waitForTimeout(data.smalltimeout)
+        await this.page.waitForTimeout(Number(process.env.smallTimeout))
         expect(this.snackbar).toBeVisible()
-        expect(this.snackbar).toHaveText("Quote deleted✕")
+        expect(this.snackbar).toHaveText(data.deleteSnackbar)
     }
 }
--- utils/helper.js
+++ utils/helper.js
@@ -1,7 +1,21 @@
-module.exports={
-    alertHandling: function (page){
-        page.on('dialog',async(dialog)=>{
-            await dialog.accept()
-        })
+const { expect } = require('@playwright/test');
+const { log } = require('console');
+
+function alertHandling(page){
+    page.on('dialog',async(dialog)=>{
+        await dialog.accept()
+    })
+}
+
+async function validateTable(rowLocator, text,childPosition) {
+    const recordsCount = await rowLocator.count();
+    for (let index = 0; index < recordsCount; index++) {
+        const value = await rowLocator.nth(index).locator(`td:nth-child(${childPosition})`).textContent();  // Change selector if needed
+        expect(value.trim()).toBe(text);
     }
+}
+
+module.exports={
+    alertHandling,
+    validateTable
 }
